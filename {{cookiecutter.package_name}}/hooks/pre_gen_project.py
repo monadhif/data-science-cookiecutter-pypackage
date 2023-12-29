cfd2de8bd838e3aaec7baa14481c3e222b658f5d
Mona Dhiflaoui <mona.dhiflaoui@gmail.com>
2023-12-29 10:11:43 +0100

    Feat: create the python environment
    Add more option to the json file and the post generation project file

--- {{cookiecutter.package_name}}/hooks/pre_gen_project.py
+++ {{cookiecutter.package_name}}/hooks/pre_gen_project.py
@@ -1,62 +1,11 @@
-import subprocess
+import re
 import sys
 
-if "{{ cookiecutter.create_virtualenv }}" != "none":
-    virtualenv_option = "{{ cookiecutter.create_virtualenv }}"
-    if virtualenv_option == "conda":
-        subprocess.check_call(
-            [
-                "conda",
-                "create",
-                "--name",
-                "{{ cookiecutter.project_slug }}",
-                "python={{ cookiecutter.python_version }}",
-            ]
-        )
-    elif virtualenv_option == "venv":
-        subprocess.check_call(
-            [
-                sys.executable,
-                "-m",
-                "venv",
-                ".venv",
-                "--prompt",
-                "{{ cookiecutter.project_slug }}",
-                "--upgrade",
-            ]
-        )
-        subprocess.check_call(
-            [".venv/bin/python", "-m", "pip", "install", "--upgrade", "pip"]
-        )
-        subprocess.check_call([".venv/bin/python", "-m", "pip", "install", "wheel"])
-        subprocess.check_call(
-            [".venv/bin/python", "-m", "pip", "install", "setuptools"]
-        )
-        subprocess.check_call(
-            [".venv/bin/python", "-m", "pip", "install", "virtualenv"]
-        )
-        subprocess.check_call(
-            [
-                ".venv/bin/python",
-                "-m",
-                "pip",
-                "install",
-                f"python=={{ cookiecutter.python_version }}",
-            ]
-        )
-    elif virtualenv_option == "virtualenv":
-        subprocess.check_call(
-            ["virtualenv", "-p", f"python{{ cookiecutter.python_version }}", ".venv"]
-        )
-        subprocess.check_call(
-            [".venv/bin/python", "-m", "pip", "install", "--upgrade", "pip"]
-        )
-        subprocess.check_call([".venv/bin/python", "-m", "pip", "install", "wheel"])
-        subprocess.check_call(
-            [".venv/bin/python", "-m", "pip", "install", "setuptools"]
-        )
-        subprocess.check_call(
-            [".venv/bin/python", "-m", "pip", "install", "virtualenv"]
-        )
-else:
-    print("Virtual environment creation skipped.")
+project_name = "{{ cookiecutter.project_name }}"
+
+# Check if project_name contains only letters, numbers, hyphens or underscores
+if not re.match(r"^[\w-]+$", project_name):
+    print(
+        f"ERROR: The project name ({project_name}) is not valid. It must contain only letters, numbers, hyphens or underscores."
+    )
+    sys.exit(1)
